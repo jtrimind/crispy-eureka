5e43a5819a6bf7ce43e404a00da9f9355e1419a6
jtrimind <trimind@naver.com>
2020-10-09 15:48:10 +0900

    Updates

---
+++ precache-manifest.94d5251c32c16a89c41f0ef8d4536c59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "450eca1003d207c55b814cedc3768ef9",
+    "url": "/crispy-eureka/index.html"
+  },
+  {
+    "revision": "94e478ed5a199a97ddc2",
+    "url": "/crispy-eureka/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "f5e2d2c0efa2cd83df3b",
+    "url": "/crispy-eureka/static/js/2.b10ab929.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crispy-eureka/static/js/2.b10ab929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94e478ed5a199a97ddc2",
+    "url": "/crispy-eureka/static/js/main.c02edb68.chunk.js"
+  },
+  {
+    "revision": "9b774c85f4e99a8e71fb",
+    "url": "/crispy-eureka/static/js/runtime-main.a9d75d56.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/crispy-eureka/static/media/logo.5d5d9eef.svg"
+  }
+]);
