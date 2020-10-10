bb04c4f211fa297178aafade52e7cedbbf9d2403
jtrimind <trimind@naver.com>
2020-10-10 13:14:03 +0900

    Add dragend listener

--- src/Map.js
+++ src/Map.js
@@ -21,6 +21,17 @@ class Map extends React.Component {
                 };
 
                 const map = new window.kakao.maps.Map(container, options);
+                kakao.maps.event.addListener(map, 'dragend', function() {        
+    
+                    // 지도 중심좌표를 얻어옵니다 
+                    var latlng = map.getCenter(); 
+                    
+                    var message = '변경된 지도 중심좌표는 ' + latlng.getLat() + ' 이고, ';
+                    message += '경도는 ' + latlng.getLng() + ' 입니다';
+                    
+                    console.log(message);
+                    
+                });
 
             });
         };
