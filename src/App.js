98ddbf9573109692faaffa07d0a4aed8fed35d5f
junmin kim <trimind@naver.com>
2020-10-28 22:55:08 +0900

    Apply hook at App.js

--- src/App.js
+++ src/App.js
@@ -1,44 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Map from './Map';
 import Parmacy from './Parmarcy';
 
-class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            latlng: [0.0, 0.0],
-        };
-        this.onPositionChanged = this.onPositionChanged.bind(this);
-    }
+function App() {
+  const [latlng, setLatlng] = useState([0, 0]);
 
-    onPositionChanged(lat, lng) {
-        this.setState({latlng: [lat, lng]});
-        console.log("position changed", lat, lng);
-    }
-
-    componentDidMount() {
-        if ("geolocation" in navigator) {
-            console.log("Available");
-            navigator.geolocation.getCurrentPosition(function (position) {
-                console.log("Latitude is :", position.coords.latitude);
-                console.log("Longitude is :", position.coords.longitude);
-            }, function (error) {
-                console.error("Error Code = " + error.code + " - " + error.message);
-            });
-        } else {
-            console.log("Not Available");
-        }
-    }
-
-    render() {
-        return (
-            <div className="app">
-                <Map onPositionChanged={this.onPositionChanged}></Map>
-                <Parmacy latlng={this.state.latlng} ></Parmacy>
-            </div>
-        );
-    }
+  return (
+    <div className="app">
+      <Map onPositionChanged={(lat, lng) => {
+        console.log("position changed: " + lat + ' ' + lng);
+        setLatlng([lat, lng]);
+      }} />
+      <Parmacy latlng={ latlng } />
+    </div>
+  );
 }
 
 export default App;
